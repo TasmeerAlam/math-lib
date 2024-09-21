6515e8487ee613ef217b46d5866aab5f763ff77b
TasmeerAlam <tasmeeralam@gmail.com>
2024-09-21 10:58:54 -0400

    feat: add new addition function

---
+++ src/math_lib/add2.py
@@ -0,0 +1,14 @@
+"""Addition module."""
+
+
+def add(augend: float, addend: float) -> float:
+    """Addition function.
+
+    Args:
+        augend (float): The augend of the addition.
+        addend (float): The addend of the addition.
+
+    Returns:
+        float: The sum of augend and addend.
+    """
+    return augend + addend
